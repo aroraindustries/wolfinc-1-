2c139afeffba72aba579b7281605ed99f126e65a
Prabhav <officialprabhav@gmail.com>
2017-06-26 16:18:37 +0400

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wolf Inc (1)
+//
+//  Created by Prabhav on 26/6/17.
+//  Copyright © 2017 Prabhav Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
